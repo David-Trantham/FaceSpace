8c0e959dc1826d24772f841727c26ca6efd1b3d1
Arvinwang21 <32558343+Arvinwang21@users.noreply.github.com>
2018-12-10 14:07:33 -0500

    Add files via upload

--- User.java
+++ User.java
@@ -4,6 +4,7 @@ public class User { //the user's name is a key
 	private String name;
 	private int id;
 	
+	
 	public User(String name, int id) {
 		this.name = name;
 		this.id = id;
@@ -13,7 +14,7 @@ public class User { //the user's name is a key
 	public String name() {
 		return this.name;
 	}
-  
+	
 	public int id() {
 		return this.id;
 	}
