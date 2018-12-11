818e2bc1c7c6d2e055f161d2249559e83385625f
Arvinwang21 <32558343+Arvinwang21@users.noreply.github.com>
2018-12-10 21:27:13 -0500

    Uploaded all

--- FaceSpace.java
+++ FaceSpace.java
@@ -8,62 +8,81 @@ public class FaceSpace {
 	private HashTableChained htc;
 	private Graph friends;
 	private int nextid;
-	private String pathToFile;
-	
+
 	public FaceSpace() {
 		this.nextid = 0;
 		this.htc = new HashTableChained(31);
-		this.pathToFile = "/Users/arvinwang/Desktop/NameList.txt";
 		friends = new Graph(31);
-		
+
 	}
 
-	public void AddUser(String name) {	//Adds user to the htc based on the name string
+	public void AddUser(String name) { // Adds user to the htc based on the name string
 		User newUser = new User(name, nextid++);
 		htc.put(newUser);
-		//System.out.println(htc.hash(name));
+
 	}
 
 	public User searchUser(String name) {
 		// Search for user
 		return htc.get(name);
 	}
-	
+
 	public void AddFriend(String name1, String name2) {
 		// Add friends --> add edges
-		friends.addEdge(searchUser(name1).id(),searchUser(name2).id());
+		friends.addEdge(searchUser(name1).id(), searchUser(name2).id());
 	}
-	
+
 	public void RemoveFriend(String name1, String name2) {
 		// Remove friends
-		friends.removeEdge(searchUser(name1).id(), searchUser(name2).id());
+		int id1 = searchUser(name1).id();
+		int id2 = searchUser(name2).id();
+
+		friends.removeEdge(id1, id2);
+
 	}
-	
+
 	public int degreeOfSeperation(String name1, String name2) {
 		// Find the shortest path
-		BreadthFirstPaths paths = new BreadthFirstPaths(friends, searchUser(name1).id());
-		Iterable p = paths.pathTo(searchUser(name2).id());
-		java.util.Stack s = (java.util.Stack) p;
-		//System.out.println(s);
-		return s.size()-1;
+		int id1 = searchUser(name1).id();
+		int id2 = searchUser(name2).id();
+
+		if (isConnected(name1, name2)) {
+			BreadthFirstPaths paths = new BreadthFirstPaths(friends, id1);
+			Iterable p = paths.pathTo(id2);
+			java.util.Stack s = (java.util.Stack) p;
+			return s.size() - 1;
+		} else {
+			return -1;
+		}
+	}
+
+	private boolean isConnected(String name1, String name2) {
+		ConnectedComponents cc;
+		int id1 = searchUser(name1).id();
+		int id2 = searchUser(name2).id();
+
+		cc = new ConnectedComponents(friends);
+		return (cc.id(id1) == cc.id(id2));
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException {
+
+
+	public static void main(String[] args) {
 		FaceSpace f = new FaceSpace();
-		f.AddUser("0");
-		f.AddUser("1");
-		f.AddUser("2");
-		f.AddUser("3");
+		f.AddUser("0");// 0
+		f.AddUser("1");// 1
+		f.AddUser("2");// 2
+		f.AddUser("3");// 3
 		f.AddFriend("2", "3");
 		f.AddFriend("0", "2");
-		f.AddFriend("0", "3");
+		f.AddFriend("1", "3");
 		f.AddFriend("0", "1");
-//		f.RemoveFriend("0", "1");
-//		f.RemoveFriend("1", "0");
+		f.RemoveFriend("1", "0");
+		f.RemoveFriend("1", "3");
 //		f.RemoveFriend("3", "2");
 		System.out.println(f.friends.toString());
-		System.out.println(f.degreeOfSeperation("2", "1"));
-		
+//		System.out.println(f.friends.numVertices());
+		System.out.println(f.degreeOfSeperation("1", "2"));
+
 	}
 }
-
--- Graph.java
+++ Graph.java
@@ -101,23 +101,7 @@ public class Graph {
     }
 
     public static void main(String[] args) {
-        Graph g = new Graph(6);
-        g.addEdge(0, 2);
-        g.addEdge(0, 1);
-        g.addEdge(3, 5);
-        g.addEdge(0, 5);
-        g.addEdge(1, 2);
-        g.addEdge(3, 4);
-        g.addEdge(2, 3);
-        g.addEdge(2, 4);
-
-        System.out.println(g);
-
-        if (args.length > 0) {
-           System.out.println();
-           Graph g2 = new Graph(args[0]);
-           System.out.println(g2);
-        }
+       
     }
 
 }
